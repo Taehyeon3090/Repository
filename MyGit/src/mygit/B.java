588f0e2110d1c970a83f3e2dca61b0e1734cf44e
master <master@DESKTOP-SC92107.mshome.net>
2023-12-23 19:19:45 +0900

    debB branch

--- MyGit/src/mygit/B.java
+++ MyGit/src/mygit/B.java
@@ -3,6 +3,6 @@ package mygit;
 public class B {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
-		System.out.println("This is the 1st version");
+		System.out.println("This is the 2nd version");
 	}
 }
